1f5032f8f7f4eca6f410af33b4f977e68a302167
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-09-17 19:18:18 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,150 @@
+from environs import Env
+import requests
+
+
+def predict_rub_salary(salary_from=None, salary_to=None):
+    if salary_from is not None and salary_to is not None:
+        return int((salary_from + salary_to) / 2)
+
+    if salary_from is not None:
+        return int(salary_from * 1.2)
+
+    if salary_to is not None:
+        return int(salary_to * 0.8)
+
+    return None
+
+
+def predict_rub_salary_for_hh(vacancy):
+    salary = vacancy['salary']
+
+    if salary['currency'] != 'RUR':
+        return None
+
+    return predict_rub_salary(salary['from'], salary['to'])
+
+
+def predict_rub_salary_for_superjob(vacancy):
+    return predict_rub_salary(vacancy['payment_from'], vacancy['payment_to'])
+
+
+def get_vacancies_by_language_hh(language):
+    url = 'https://api.hh.ru/vacancies'
+    params = {
+        'text': language,
+        'area': 1,
+        'only_with_salary': True,
+        'per_page': 100
+    }
+
+    has_next = True
+    vacancies = None
+    page = 0
+
+    while has_next:
+        params['page'] = page
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+
+        if vacancies is None:
+            vacancies = response.json()
+        else:
+            vacancies['items'] += response.json()['items']
+
+        page += 1
+        has_next = page < vacancies['pages']
+
+    return vacancies
+
+
+def get_vacancies_by_language_superjob(language, api_key):
+    url = 'https://api.superjob.ru/2.0/vacancies'
+    params = {
+        'town': 'Москва',
+        'keyword': language,
+        'count': 100
+    }
+    headers={'X-Api-App-Id': api_key}
+
+    has_next = True
+    vacancies = None
+    page = 0
+
+    while has_next:
+        params['page'] = page
+        response = requests.get(url, params=params, headers=headers)
+        response.raise_for_status()
+
+        if vacancies is None:
+            vacancies = response.json()
+        else:
+            vacancies['objects'] += response.json()['objects']
+
+        page += 1
+        has_next = vacancies['more']
+
+    return vacancies
+
+
+def get_average_salary_stat_hh(vacancies):
+    count = 0
+    amount = 0
+
+    for vacancy in vacancies['items']:
+        salary = predict_rub_salary_for_hh(vacancy)
+        if salary is not None:
+            count += 1
+            amount += int(salary)
+
+    return {
+        'average_salary': int(amount / count),
+        'vacancies_processed': count,
+        'vacancies_found': vacancies['found']
+    }
+
+
+def get_average_salary_stat_superjob(vacancies):
+    count = 0
+    amount = 0
+
+    for vacancy in vacancies['objects']:
+        salary = predict_rub_salary_for_superjob(vacancy)
+        if salary is not None:
+            count += 1
+            amount += int(salary)
+
+    return {
+        'average_salary': int(amount / count),
+        'vacancies_processed': count,
+        'vacancies_found': vacancies['total']
+    }
+
+
+def main():
+    env = Env()
+    env.read_env()
+    superjob_secret_key = env.str("SUPERJOB_SECRET_KEY") 
+
+    languages = ['Python', 'Java', 'Golang', 'Javascript', 'Ruby', 'PHP', 'C++', '1С']
+
+    print('+SuperJob Moscow--------+------------------+---------------------+------------------+')
+    print('| Язык программирования | Вакансий найдено | Вакансий обработано | Средняя зарплата |')
+    print('+-----------------------+------------------+---------------------+------------------+')
+    for language in languages:
+        stat = get_average_salary_stat_superjob(
+            get_vacancies_by_language_superjob('Python', api_key=superjob_secret_key)
+        )
+        print(f"| {language:22}| {stat['vacancies_found']:17}| {stat['vacancies_processed']:20}| {stat['average_salary']:17}|")
+
+    print('+HeadHunter Moscow--------+------------------+---------------------+----------------+')
+    print('| Язык программирования | Вакансий найдено | Вакансий обработано | Средняя зарплата |')
+    print('+-----------------------+------------------+---------------------+------------------+')
+    for language in languages:
+        stat = get_average_salary_stat_hh(
+            get_vacancies_by_language_hh(language)
+        )
+        print(f"| {language:22}| {stat['vacancies_found']:17}| {stat['vacancies_processed']:20}| {stat['average_salary']:17}|")
+
+
+if __name__ == '__main__':
+    main()
