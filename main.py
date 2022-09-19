f12b5a393294821342309731f9d929ee9b7daf9e
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-09-19 15:21:35 +1000

    remove unnecessary None comparisons

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from terminaltables import AsciiTable
 
 
 def predict_rub_salary(salary_from=None, salary_to=None):
-    if salary_from is not None and salary_to is not None:
+    if salary_from and salary_to:
         return int((salary_from + salary_to) / 2)
 
-    if salary_from is not None:
+    if salary_from:
         return int(salary_from * 1.2)
 
-    if salary_to is not None:
+    if salary_to:
         return int(salary_to * 0.8)
 
     return None
@@ -31,31 +31,35 @@ def predict_rub_salary_for_superjob(vacancy):
 
 def get_vacancies_by_language_hh(language):
     url = 'https://api.hh.ru/vacancies'
+    MOSCOW_AREA_ID = 1
     params = {
         'text': language,
-        'area': 1,
+        'area': MOSCOW_AREA_ID,
         'only_with_salary': True,
         'per_page': 100
     }
 
     has_next = True
-    vacancies = None
+    vacancies = []
     page = 0
+    total = 0
 
     while has_next:
         params['page'] = page
         response = requests.get(url, params=params)
         response.raise_for_status()
+        data = response.json()
 
-        if vacancies is None:
-            vacancies = response.json()
-        else:
-            vacancies['items'] += response.json()['items']
-
+        pages = data['pages']
+        vacancies += data['items']
+        total = data['found']
         page += 1
-        has_next = page < vacancies['pages']
+        has_next = page < pages
 
-    return vacancies
+    return {
+        'vacancies': vacancies,
+        'total': total
+    }
 
 
 def get_vacancies_by_language_superjob(language, api_key):
@@ -68,39 +72,42 @@ def get_vacancies_by_language_superjob(language, api_key):
     headers={'X-Api-App-Id': api_key}
 
     has_next = True
-    vacancies = None
+    vacancies = []
     page = 0
+    total = 0
 
     while has_next:
         params['page'] = page
         response = requests.get(url, params=params, headers=headers)
         response.raise_for_status()
+        data = response.json()
 
-        if vacancies is None:
-            vacancies = response.json()
-        else:
-            vacancies['objects'] += response.json()['objects']
-
+        more = data['more']
+        vacancies += data['objects']
+        total = data['total']
         page += 1
-        has_next = vacancies['more']
+        has_next = more
 
-    return vacancies
+    return {
+        'vacancies': vacancies,
+        'total': total
+    }
 
 
 def get_average_salary_stat_hh(vacancies):
     count = 0
     amount = 0
 
-    for vacancy in vacancies['items']:
+    for vacancy in vacancies['vacancies']:
         salary = predict_rub_salary_for_hh(vacancy)
-        if salary is not None:
+        if salary:
             count += 1
             amount += int(salary)
 
     return {
-        'average_salary': int(amount / count),
+        'average_salary': int(amount / count) if count > 0 else 0,
         'vacancies_processed': count,
-        'vacancies_found': vacancies['found']
+        'vacancies_found': vacancies['total']
     }
 
 
@@ -108,14 +115,14 @@ def get_average_salary_stat_superjob(vacancies):
     count = 0
     amount = 0
 
-    for vacancy in vacancies['objects']:
+    for vacancy in vacancies['vacancies']:
         salary = predict_rub_salary_for_superjob(vacancy)
-        if salary is not None:
+        if salary:
             count += 1
             amount += int(salary)
 
     return {
-        'average_salary': int(amount / count),
+        'average_salary': int(amount / count) if count > 0 else 0,
         'vacancies_processed': count,
         'vacancies_found': vacancies['total']
     }
@@ -128,7 +135,7 @@ def print_salary_info_superjob(languages, api_key):
 
     for language in languages:
         stat = get_average_salary_stat_superjob(
-            get_vacancies_by_language_superjob('Python', api_key=api_key)
+            get_vacancies_by_language_superjob(language, api_key=api_key)
         )
         table_data.append(
             (language, stat['vacancies_found'], stat['vacancies_processed'], stat['average_salary'])
@@ -145,7 +152,7 @@ def print_salary_info_hh(languages):
 
     for language in languages:
         stat = get_average_salary_stat_hh(
-            get_vacancies_by_language_hh('Python')
+            get_vacancies_by_language_hh(language)
         )
         table_data.append(
             (language, stat['vacancies_found'], stat['vacancies_processed'], stat['average_salary'])
